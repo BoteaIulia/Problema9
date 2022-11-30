2f66fcad238b0a3c8c252c899fb001cbc08d9254
botea <botea@IULIA>
2022-11-30 13:22:26 +0200

    Add project files.

---
+++ Problema9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public static class Globals
+{
+	public static void afisareDivizori(int x)
+	{
+
+		for (int i = 2; i <= x / 2; i++)
+		{
+			if (x % i == 0)
+			{
+				Console.Write(i);
+				Console.Write(" ");
+			}
+		}
+	}
+
+	internal static void Main()
+	{
+		Console.Write("Afisati toti divizorii numarului N");
+		Console.Write("\n");
+		int n;
+		Console.Write("N = ");
+		n = Convert.ToInt32(Console.ReadLine());
+		afisareDivizori(n);
+	}
+}
